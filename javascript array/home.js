4304efda861748d9746e44ff6ead1b79307bc548
elxan-code <92145329+elxan-code@users.noreply.github.com>
2021-12-04 18:36:43 +0400

    Add files via upload

---
+++ javascript array/home.js
@@ -0,0 +1,69 @@
+//#region task 1 
+// let arr1 = [1,3,5];
+// let arr2=[2,4,6];
+// let newarr=arr1.concat(arr2);
+//  console.log(newarr);
+ //#endregion
+ //#region task2
+//  function aylar(number){
+//     switch(number){
+//         case 1:
+//         return "yanvar";
+//         break;
+//         case 2:
+//         return "fevral";
+//         break;
+//         case 3:
+//         return "mart";
+//         break;
+//         case 4:
+//         return "aprel";
+//         break;
+//         case 5:
+//         return "May";
+//         break;
+//     }
+// }
+// let input= prompt();
+// console.log(aylar(parseInt(input)));
+//#endregion
+//#region tak 6
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+//  arr.forEach(element => {
+//     console.log(element)
+// }); //a
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+// arr.push(15)
+// arr.forEach(element => {
+//     console.log(element)
+// });//c
+
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+// arr.shift(23)
+// arr.forEach(element => {
+//     console.log(element)
+// }) //d
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+// arr.sort((a,b)=>a-b);
+// arr.forEach(element => {
+//     console.log(element)
+// });
+// console.log("------------------")
+// console.log(arr[arr.length-1]) //e
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+//  arr.splice(3,0,43);
+// console.log(arr); //f
+// let arr =[23,25,85,89,45,76,7,41,2,12];
+// console.log(arr.splice(3)); //g
+// let arr =[23,25,85,89,45,76,7,41,2,12]
+//  arr.forEach(element => {
+//     console.log(element) //h
+// #endregion
+
+
+
+
+
+
+
+
